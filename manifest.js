9e222580bb6e2feb9abe7281eeb61361c193ebd4
Sky <3504113140@qq.com>
2021-10-21 15:07:57 +0800

    修改reg js路径

--- manifest.js
+++ manifest.js
@@ -1,11 +1,7 @@
 /* Site resources file. Created at 2021-10-17 21:06:17.834 */  
 var manifest = [  
-'img/bg-down.png',  
-'img/bg-top.png',  
-'img/bg.png',  
-'img/down-balloon.png',  
-'img/heart2.png',  
-'img/heart3.png',  
+
+
 'img/home.png',  
 'img/logo.png',  
 ];  
