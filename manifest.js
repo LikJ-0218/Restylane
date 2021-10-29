fd33c07478299cd3782e211fa9bcd14f54637c06
LikJ-218 <3504113140@qq.com>
2021-10-29 14:17:56 +0800

    删除

--- manifest.js
+++
@@ -1,9 +0,0 @@
-/* Site resources file. Created at 2021-10-17 21:06:17.834 */  
-var manifest = [  
-'img/bg-down.png',  
-'img/bg-top.png',  
-'img/bg.png',  
-'img/down-balloon.png',  
-'img/home.png',  
-'img/logo.png',  
-];  
